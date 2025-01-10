5b7c100d5bbd842f7b5e13b562ec69d1d91b7e55
Shubham Sholet <shubhamsholet@gmail.com>
2025-01-10 17:13:27 +0530

    read me file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  // firebaseConfig: {
+  //   projectId: "carpool-4bac6",
+  //   appId: "1:131793287583:web:b600ce74c07df536e765aa",
+  //   storageBucket: "carpool-4bac6.appspot.com",
+  //   apiKey: "AIzaSyDOzD2DHJnnZNm7R23awls9_dmqht-PV2E",
+  //   authDomain: "carpool-4bac6.firebaseapp.com",
+  //   messagingSenderId: "131793287583",
+  //   measurementId: "G-4PDKQ35BBJ",
+  // },
+  firebaseConfig: {
+    apiKey: "AIzaSyCD-kC15-8p3Drs4xfB8_Mirl0Smb2gTwQ",
+    authDomain: "travelapp-7a71b.firebaseapp.com",
+    projectId: "travelapp-7a71b",
+    storageBucket: "travelapp-7a71b.firebasestorage.app",
+    messagingSenderId: "1057265598891",
+    appId: "1:1057265598891:web:cda324ddc843a7415252ee"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,62 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // firebaseConfig: {
+  //   projectId: "carpool-4bac6",
+  //   appId: "1:131793287583:web:b600ce74c07df536e765aa",
+  //   storageBucket: "carpool-4bac6.appspot.com",
+  //   apiKey: "AIzaSyDOzD2DHJnnZNm7R23awls9_dmqht-PV2E",
+  //   authDomain: "carpool-4bac6.firebaseapp.com",
+  //   messagingSenderId: "131793287583",
+  //   measurementId: "G-4PDKQ35BBJ",
+  // },
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCD-kC15-8p3Drs4xfB8_Mirl0Smb2gTwQ",
+    authDomain: "travelapp-7a71b.firebaseapp.com",
+    projectId: "travelapp-7a71b",
+    storageBucket: "travelapp-7a71b.firebasestorage.app",
+    messagingSenderId: "1057265598891",
+    appId: "1:1057265598891:web:cda324ddc843a7415252ee"
+  }
+
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+
+
+
+
+// updated part
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCD-kC15-8p3Drs4xfB8_Mirl0Smb2gTwQ",
+//   authDomain: "travelapp-7a71b.firebaseapp.com",
+//   projectId: "travelapp-7a71b",
+//   storageBucket: "travelapp-7a71b.firebasestorage.app",
+//   messagingSenderId: "1057265598891",
+//   appId: "1:1057265598891:web:cda324ddc843a7415252ee"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
